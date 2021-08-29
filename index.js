eb1e301706cad3534e0411c2d1ca9602e3054e90
Malcolm Heggie <mheggie@DESKTOP-8IG0D17.localdomain>
2021-08-28 22:16:51 -0400

    commit tree dff7270e35e7d2ae12884d61ac1b6cb40fc333ac

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+    if(someValue <= 400) {
+      return 'This one is on me!'
+    }else if(someValue >= 2000 && someValue <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+    }else if(someValue >= 2500) {
+      return 'No can do.'
+    } // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(NYC){
+    return NYC === 'NYC' ? 'Ok, sounds good.' : 'No go.' // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+  switch(generous) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  } // Write your code here!
 }
